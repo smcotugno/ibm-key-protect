898175536624a4dbe4c4506628d34676c3cf90de
Steve Cotugno <steve.cotugno@us.ibm.com>
2020-03-12 20:23:55 -0700

    corrected properties

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 
 output "keyName" {
-  value = "${ibm_kp_key.test.keys.key_name}"
+  value = "${ibm_kp_key.test.key_name}"
 }
 
 output "keyId" {
-  value = "${ibm_kp_key.test.keys.key_id}"
+  value = "${ibm_kp_key.test.key_id}"
 }
